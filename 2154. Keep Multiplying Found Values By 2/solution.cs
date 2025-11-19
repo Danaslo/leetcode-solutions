4cc5232becb9b015aada34061dc5b1f093f3b292
Daniel Astorga López <astorgalopezd89@gmail.com>
2025-11-19 16:20:57 +0100

    Solution to problem 2154

---
+++ 2154. Keep Multiplying Found Values By 2/solution.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int FindFinalValue(int[] nums, int original) {
+        Dictionary<int, int> numMap = new Dictionary<int, int>();
+        foreach(int number in nums)
+            numMap[number] = number;
+        while(numMap.ContainsKey(original) == true)
+            original *=2;
+        return original;
+    }
+}
